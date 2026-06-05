1d45231fe1834c401e025be501d1d87911582626
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2026-06-05 15:11:14 +0800

    Change API Gateway integration type from AWS_PROXY to AWS

--- infra_terraform/main.tf
+++ infra_terraform/main.tf
@@ -178,7 +178,7 @@ resource "aws_api_gateway_integration" "lambda" {
   resource_id             = aws_api_gateway_resource.price_per_meter.id
   http_method             = aws_api_gateway_method.post_price.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = aws_lambda_function.calculate_cost.invoke_arn
 }
 
